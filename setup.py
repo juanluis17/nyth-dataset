ca46f8bd2268d46da2c0a8415b8ea286f4bf3724
juanluis1702 <juanluis1702@gmail.com>
2023-06-02 16:56:41 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nyth_dataset',
+    version='0.0.1',
+    description='',
+    url='https://github.com/juanluis17/nyth-dataset',
+    author='Juan-Luis García-Mendoza',
+    author_email='jluisgm90@gmail.com',
+    license='Apache Software License',
+    install_requires=['pandas',
+                      ],
+)
